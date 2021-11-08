febbd9807b56127b5ce033af9bb147c16c18f53d
Bauther <91104313+Bauther@users.noreply.github.com>
2021-11-08 13:36:42 -0600

    Correción

--- APP/view/seguridad.php
+++ APP/view/seguridad.php
@@ -9,6 +9,9 @@
 
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<meta charset="utf-8">
+	<!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
 </head>
 <body id="bodyseg">
 	<?php
