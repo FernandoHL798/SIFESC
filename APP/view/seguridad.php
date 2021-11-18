4a81817b71d989cca9137442063a87bf85eacbf3
Bauther <91104313+Bauther@users.noreply.github.com>
2021-11-18 15:15:47 -0600

    Correccion

--- APP/view/seguridad.php
+++ APP/view/seguridad.php
@@ -1,41 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<link rel="stylesheet" type="text/css" href="../../css/styles_menu.css">
-
-	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<meta charset="utf-8">
-	<!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Menú responsivo UNAM</title>  
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../css/styles_menu.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <title>Seguridad</title>
 </head>
 <body id="body">
-	<?php
-include "./includes/sidebar.php";
-	?>
-
-
-	<header>
-		<div class="icon_menu">
-			<i class='bx bx-menu' id="btn"></i>
-		</div>
-		<div class="UNAMFESC">
-
-			<title1 class="UNAM"><center>UNIVERSIDAD NACIONAL AUTONOMA DE MÉXICO</center></title1>
-			<title2 class="fesc"><center><br>Facultad de Estudios Superiores Cuautitlán</center></title2>
-
-			</div>
-	</header>
+    <?php
+    include "./includes/sidebar.php";
+    ?>
+    <div class="home_content">
+        <header>
+            <div class="icon_menu">
+                <i class='bx bx-menu' id="btn"></i>
+            </div>
+            <div class="titulo">
+                <title1 class="UNAM"><center></center></title1>
+                <title2 class="fesc"><center><br></center></title2>
+            </div>
+        </header>
+	
 
 
 
 
 	<!--              AQUI VA EL CONTENIDO                -->
-	<div class="home_content">
+	
 		<div class="text"> 
             <form>
                 <div class="">
@@ -98,9 +93,9 @@ include "./includes/sidebar.php";
                             </div>
                     </div>
                         <div class="row">
-                            <div class="col-3"></div>
-                            <div class="col-lg-6">
-                                <div class="form-floating mx-auto">
+                            
+                            <div class="col-6 mx-auto">
+                                <div class="form-floating">
                                     <select class="form-select" id="floatingSelect">
                                         <option hidden=""></option>
                                         <option value="1">¿Como se llama su mamá?</option>
@@ -108,7 +103,7 @@ include "./includes/sidebar.php";
                                         <option value="3">¿Cual es el nombre de su escuela primaria?</option>
                                     </select>
                                         <label for="floatingSelect"><FONT SIZE=3>Seleccione una pregunta secreta</font></label>
-                                </div>
+                                
 
                             </div>
                         </div>
@@ -161,7 +156,7 @@ include "./includes/sidebar.php";
 
 
 
-
+</div>
 
 
 
