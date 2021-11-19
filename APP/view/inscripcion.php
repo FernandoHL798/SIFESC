d9439e3d46f4084d75d6c4624ed0bea790a45542
EmmanuelGntleman <emmanuel_th_2698@hotmail.com>
2021-11-19 02:54:05 -0600

    SIDEBAR 2

--- APP/view/inscripcion.php
+++ APP/view/inscripcion.php
@@ -214,10 +214,30 @@ include "./includes/sidebar.php";
 			      </div>
 			      <div class="modal-body">
 			        AQUI IRA LA INSCRUPCIÓN
-			      </div>
+
+
+			        <!-- CONTENIDO DEL MODAL-->
+<form>
+  <div class="pruebass">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
 			      <div class="modal-footer">
-			        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-			        <button type="button" class="btn btn-primary">Save changes</button>
+			        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+			        <button type="button" class="btn btn-primary">Guardar cambios</button>
 			      </div>
 			    </div>
 		 	</div>
