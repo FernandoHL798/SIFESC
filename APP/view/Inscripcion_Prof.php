a48f00ee3cd41b7b3ef2c1937af2f8836df641c7
Bauther <91104313+Bauther@users.noreply.github.com>
2021-11-19 20:04:26 -0600

    Saturacion_Prof

--- APP/view/Inscripcion_Prof.php
+++ APP/view/Inscripcion_Prof.php
@@ -3,36 +3,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Menú responsivo UNAM</title>
-	<link rel="stylesheet" type="text/css" href="../../css/styles_menu.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
+    <title>Menú responsivo UNAM</title>
+    <link rel="stylesheet" type="text/css" href="../../css/styles_menu.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <meta charset="utf-8">
 </head>
 <body id="body">
-	<?php
+    <?php
 include "./includes/sidebar.php";
-	?>
+    ?>
 
 
-	<header>
-		<div class="icon_menu">
-			<i class='bx bx-menu' id="btn"></i>
-		</div>
-		<div class="UNAMFESC">
+    <header>
+        <div class="icon_menu">
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <div class="UNAMFESC">
 
-			<title1 class="UNAM"><center>UNIVERSIDAD NACIONAL AUTONOMA DE MÉXICO</center></title1>
-			<title2 class="fesc"><center><br>Facultad de Estudios Superiores Cuautitlán</center></title2>
+            <title1 class="UNAM"><center>UNIVERSIDAD NACIONAL AUTONOMA DE MÉXICO</center></title1>
+            <title2 class="fesc"><center><br>Facultad de Estudios Superiores Cuautitlán</center></title2>
 
-			</div>
-	</header>
-	<!--              AQUI VA EL CONTENIDO                -->
-	<div class="home_content">
-		<div class="text">
- <form>              
-            <div class="">
+            </div>
+    </header>
+    <!--              AQUI VA EL CONTENIDO                -->
+    <div class="home_content" class="mb-4">
+        <div class="text">
+ <form class="mb-4">              
+            <div class="mb-4">
                 <div class="row">
                     <div class="">
                         <a href="http://localhost/SIFESC/APP/view/index.php"> 
@@ -46,12 +46,13 @@ include "./includes/sidebar.php";
                     <thead>
                         <tr class="text-center">
                             <th style="width: 100px;">Asig</th>
-                            <th style="width: 600px;">Nombre</th>
+                            <th style="width: 400px;">Nombre</th>
                             <th style="width: 106px;">Gpo</th>
                             <th style="width: 70px;">Se</th>
                             <th style="width: 70px;">Cr</th>
                             <th style="width: 93px;">Cup</th>
                             <th style="width: 70px;">In</th>
+                            <th style="width: 140px;">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -63,6 +64,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>28</td>
                             <td>2</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0147</td>
@@ -72,6 +77,11 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>25</td>
                             <td>24</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button>
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button>
+                                </button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0162</td>
@@ -81,6 +91,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>35</td>
                             <td>29</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -88,12 +102,13 @@ include "./includes/sidebar.php";
                     <thead>
                         <tr class="text-center">
                             <th style="width: 100px;">Asig</th>
-                            <th style="width: 600px;">Nombre</th>
+                            <th style="width: 400px;">Nombre</th>
                             <th style="width: 106px;">Gpo</th>
                             <th style="width: 70px;">Se</th>
                             <th style="width: 70px;">Cr</th>
                             <th style="width: 93px;">Cup</th>
                             <th style="width: 70px;">In</th>
+                            <th style="width: 140px;">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -105,6 +120,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>30</td>
                             <td>19</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0139</td>
@@ -114,6 +133,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>27</td>
                             <td>25</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -121,12 +144,13 @@ include "./includes/sidebar.php";
                     <thead>
                         <tr class="text-center">
                             <th style="width: 100px;">Asig</th>
-                            <th style="width: 600px;">Nombre</th>
+                            <th style="width: 400px;">Nombre</th>
                             <th style="width: 106px;">Gpo</th>
                             <th style="width: 70px;">Se</th>
                             <th style="width: 70px;">Cr</th>
                             <th style="width: 93px;">Cup</th>
                             <th style="width: 70px;">In</th>
+                            <th style="width: 140px;">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -138,6 +162,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>25</td>
                             <td>13</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0139</td>
@@ -147,6 +175,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>27</td>
                             <td>26</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -154,12 +186,13 @@ include "./includes/sidebar.php";
                     <thead>
                         <tr class="text-center">
                             <th style="width: 100px;">Asig</th>
-                            <th style="width: 600px;">Nombre</th>
+                            <th style="width: 400px;">Nombre</th>
                             <th style="width: 106px;">Gpo</th>
                             <th style="width: 70px;">Se</th>
                             <th style="width: 70px;">Cr</th>
                             <th style="width: 93px;">Cup</th>
                             <th style="width: 70px;">In</th>
+                            <th style="width: 140px;">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -171,19 +204,24 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>23</td>
                             <td>1</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                     </tbody>
                 </table>
-                <table class="table table-bordered  display nowrap table-responsive" cellspacing="0" width="100%">
+                <table class="table table-bordered  display nowrap table-responsive" cellspacing="0" width="100%" class="mb-4">
                     <thead>
                         <tr class="text-center">
                             <th style="width: 100px;">Asig</th>
-                            <th style="width: 600px;">Nombre</th>
+                            <th style="width: 400px;">Nombre</th>
                             <th style="width: 106px;">Gpo</th>
                             <th style="width: 70px;">Se</th>
                             <th style="width: 70px;">Cr</th>
                             <th style="width: 93px;">Cup</th>
                             <th style="width: 70px;">In</th>
+                            <th style="width: 140px;">Acciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -195,6 +233,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>23</td>
                             <td>2</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0019</td>
@@ -204,6 +246,10 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>30</td>
                             <td>22</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
                         </tr>
                         <tr class="text-center">
                             <td>0020</td>
@@ -213,34 +259,38 @@ include "./includes/sidebar.php";
                             <td>8</td>
                             <td>23</td>
                             <td>14</td>
+                            <td>
+                                <button type="button" class="btn btn-danger"><i class='bx bx-trash'></i></button> 
+                                <button type="button" class="btn btn-success"><i class='bx bx-refresh'></i></button> 
+                            </td>
+
                         </tr>
+
                     </tbody>
+
                 </table>
-
-
-
-
-
-
+                <!-- Se utiliza para darle espacio al final de la tabla -->
+                <div class="mt-3 mb-2"></div>
+                <div class="d-grid gap-2 col-3 mx-auto">
+                <p class="mt-3 mb-2"></p>
+                </div>
             </div>
         </form>   
-
-            
-		</div>
-	</div>
+        </div>
+    </div>
 
 
-	<script>
-		let btn = document.querySelector("#btn");
-		let sidebar = document.querySelector(".sidebar");
+    <script>
+        let btn = document.querySelector("#btn");
+        let sidebar = document.querySelector(".sidebar");
 
-		btn.onclick = function(){
-		sidebar.classList.toggle("active");
-		}
+        btn.onclick = function(){
+        sidebar.classList.toggle("active");
+        }
 
-	</script>
+    </script>
 
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
