f471bbf38325ed777c23961010b67407ec360801
Erick Arcos <91168394+Edgeman011@users.noreply.github.com>
2021-11-22 12:01:07 -0600

    Boton imprimir "contenido"
    boton imprimir en ConsultaInscripcion
    Imprime ID="contenido" sin menú

--- APP/view/ConsultaInscripcion.php
+++ APP/view/ConsultaInscripcion.php
@@ -47,7 +47,7 @@ include "./includes/sidebar2.php";
 	<div class="titulo">
 	<center>SISTEMA DE INSCRIPCIÓN</center>
 	</div>
-	<div class="container">
+	<div class="container" id="contenido">
 		<div class="row">
 			<div class="col-lg-12">
 				<table id="example" class="table table-bordered">
@@ -178,9 +178,7 @@ include "./includes/sidebar2.php";
 
 	<div class="col-lg-12 mt-4">
 		<div  class="d-grid gap-2 col-11 mx-auto">
-			<button class="btn btn-secondary" type="button" onclick="javascript:print('container')">
-			  IMPRIMIR
-			</button>
+			<button class="btn btn-secondary" type="button" onclick="printData()">IMPRIMIR</button>
 
 			<button class="btn btn-secondary" type="button" class="btn btn-outline-success mt-1">SALIR</button>
 		</div>
@@ -212,6 +210,25 @@ include "./includes/sidebar2.php";
 				sidebar.classList.toggle("close");
 			});
 
+
+		</script>
+
+		<script>
+
+			function printData()
+				{
+				   var divToPrint=document.getElementById("contenido");
+				   newWin= window.open("");
+				   newWin.document.write(divToPrint.outerHTML);
+				   newWin.document.write('<link rel="stylesheet" type="text/css" href="../../css/styles_menu.css">');
+				   newWin.document.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">');
+				   newWin.print();
+				   newWin.close();
+				}
+
+				$('Imprimir').on('click',function(){
+				printData();
+				})
 
 		</script>
 </body>
