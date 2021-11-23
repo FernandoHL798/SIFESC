50e63a1935508a61000c3ee598c7b9c3df2240d5
LuizPichardo <90637231+LuizPichardo@users.noreply.github.com>
2021-11-22 22:55:01 -0600

    update_Cambio_clave
    se actualiza el cambio clave

--- APP/view/cambio_clave.php
+++ APP/view/cambio_clave.php
@@ -65,54 +65,14 @@
 						Cambiar contraseña
 					</button>
 
-					<!-- Modal -->
-					<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-						<div class="modal-dialog" role="document">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="exampleModalLongTitle">Confirmar cambio</h5>
-									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-								</div>
-								<div class="modal-body">
-									Estas a punto de cambiar tu contraseña actual, ¿Estas seguro de que deseas continuar?
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-primary" data-dismiss="modal">Regresar</button>
-									<button type="button" class="btn btn-danger">Continuar</button>
-								</div>
-							</div>
-						</div>
-					</div>
+					<!-- Modal confirmacion -->
+
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<script>
-		
-		let arrow = document.querySelectorAll(".arrow");
-		console.log(arrow);
-		for (var i = 0; i < arrow.length; i++) {
-			arrow[i].addEventListener("click",(e)=>{
-				let arrowParent = e.target.parentElement.parentElement;
-				
-				arrowParent.classList.toggle("showMenu");
-
-			});
-		}
-
-		let sidebar = document.querySelector(".sidebar");
-		let sidebarBtn = document.querySelector(".bx-menu");
-		console.log(sidebarBtn);
-
-		sidebarBtn.addEventListener("click", ()=>{
-			sidebar.classList.toggle("close");
-		});
-
-
-	</script>
+		<script src="../tools/sidebar.js"></script>
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -133,6 +93,7 @@
 			} );
 
 		</script>
+		<?php include "./modal/modal-confirma-cambio-contra.php"; ?>
 
 	</body>
 	</html>
---
+++ APP/view/modal/modal-confirma-cambio-contra.php
@@ -0,0 +1,20 @@
+					<!-- Modal Confirmacion Cambio Contraseña -->
+					<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLongTitle">Confirmar cambio</h5>
+									<button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									Estas a punto de cambiar tu contraseña actual, ¿Estas seguro de que deseas continuar?
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-primary" data-dismiss="modal">Regresar</button>
+									<button type="button" class="btn btn-danger">Continuar</button>
+								</div>
+							</div>
+						</div>
+					</div>
