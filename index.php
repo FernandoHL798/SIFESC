248929c4e5a41b14e5411d206cb729b251ff94f8
LuizPichardo <90637231+LuizPichardo@users.noreply.github.com>
2021-11-25 01:27:32 -0600

    LoginPrincipal

--- index.php
+++ index.php
@@ -50,9 +50,7 @@
               <div class="mb-4">
                 <label for="exampleInputPassword1" class="form-label font-weight-bold">Contraseña</label>
                 <input type="password" class="form-control mb-2" placeholder="Ingresa tu contraseña" id="exampleInputPassword1">
-                <a href="#" id="emailHelp" class="form-text text-muted text-decoration-none">¿Has olvidado tu contraseña?</a>
-
-                
+                <a href="#" id="emailHelp" class="form-text text-muted text-decoration-none" data-bs-toggle="modal" data-bs-target="#validarDatos">¿Has olvidado tu contraseña?</a>
                 
               </div>
               <button type="submit" class="btn btn-primary w-100">Iniciar sesión</button>
@@ -73,5 +71,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
+    <!-- PHP DE MODALES -->
+    <?php include "./APP/VIEW/modal/modal-valida-datos-recuperacion.php"; ?>
+
   </body>
 </html>
