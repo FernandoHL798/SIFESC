b2328a4786ec3f1710d51db620bbea6c5493eee7
Fernando <lfernandohl901@gmail.com>
2021-11-24 22:39:49 -0600

    Avance service saturacion

--- APP/view/saturacion.php
+++ APP/view/saturacion.php
@@ -42,8 +42,8 @@ include "./includes/sidebar2.php";
                             <th style="width: 70px;">In</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr class="text-center">
+                    <tbody id="tbl-asignaciones">
+                        <!--<tr class="text-center">
                             <td data-label="Clave Asig">0017</td>
                             <td data-label="Nombre">SEM DE COMERCIO ELECTRONICO II</td>
                             <td data-label="Gpo">1001</td>
@@ -202,7 +202,7 @@ include "./includes/sidebar2.php";
                             <td data-label="Cr">8</td>
                             <td data-label="Cup">23</td>
                             <td data-label="In">14</td>
-                        </tr>
+                        </tr> -->
                     </tbody>
                 </table>
                 
@@ -227,19 +227,19 @@ include "./includes/sidebar2.php";
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!--   Datatables-->
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.20/datatables.min.js"></script>  
     <!-- extension responsive -->
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-
     <script>
         $(document).ready(function() {
             $('#example').DataTable({
                 responsive: true
             });
         } );  
-
     </script>
+    <script src="../service/saturacion.js"></script>
 
 </body>
 </html>
