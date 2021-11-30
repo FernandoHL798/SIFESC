8d6428c2ac9ae29248b71ee377e885e8122a7e47
Luz Espinoza <luzespinoza250198@gmail.com>
2021-11-30 16:00:25 -0600

    prueba_luz

---
+++ APP/model/Facu_luz.php
@@ -0,0 +1,31 @@
+<?php
+include_once "CONEXION.php";
+class FACULTAD extens CONEXION{
+	private $id_facultad;
+	private $nombre_facultad;
+
+	public function getIdFacultad(){
+		return $this->id_facultad;
+	}
+
+	public function setIdFacultad($id_facultad):void{
+		$this->id_facultad=$id_facultad;	
+	}
+
+	public function getNombreFacultad(){
+		return $this->nombre_facultad;
+	}
+
+	public function setNombreFacultad($nombre_facultad):void{
+		$this->nombre_facultad=$nombre_facultad;	
+	}
+
+	public function queryConsultaFacultad(){
+		$query="select `id_facultad, nombre_facultad` from `facultad`";
+		$this->connect();
+		$resultado=$this->getData($query);
+		$this->close();
+		return $resultado;
+	}
+
+}
