15a2911cd1d49436c9d5fdb42932588169862621
Luz Espinoza <luzespinoza250198@gmail.com>
2021-12-21 13:09:50 -0600

    Avance_Planes de estudio

--- APP/service/lista-plandeestudios.js
+++ APP/service/lista-plandeestudios.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     listaplandeestudios();
     listaplandeestudiosasig();
     listaPlanesAgAlumno();
+    listaplandeestudiosAdmin();
 });
 
 
@@ -12,7 +13,7 @@ function listaplandeestudios(){
         data : {},
         success: function (response){
             let PLANESESTUDIO =JSON.parse(response);
-            console.log(PLANESESTUDIO);
+            //console.log(PLANESESTUDIO);
             let template="";
             PLANESESTUDIO.forEach(planes=>{
                 template += `<tr class="text-center">
@@ -37,7 +38,7 @@ function listaplandeestudiosasig(){
         data : {},
         success: function (response){
             let PLANESESTUDIO =JSON.parse(response);
-            console.log(PLANESESTUDIO);
+            //console.log(PLANESESTUDIO);
             let template="";
             PLANESESTUDIO.forEach(planes=>{
                 template += `<tr class="text-center">
@@ -62,7 +63,7 @@ function listaPlanesAgAlumno(){
         data : {},
         success: function (response){
             let PLANESESTUDIO =JSON.parse(response);
-            console.log(PLANESESTUDIO);
+            //console.log(PLANESESTUDIO);
             let template="";
             PLANESESTUDIO.forEach(planes=>{
                 template += `<tr class="text-center">
@@ -75,5 +76,43 @@ function listaPlanesAgAlumno(){
             });
             $("#tbl-lista-planes-alumnos").html(template);
         }
+    });
+}
+
+/*FUNCION PARA LISTA PLANES DE ESTUDIO EN ADMIN*/
+function listaplandeestudiosAdmin(){
+    $.ajax({
+        url: "../webhook/lista_planestudios.php",
+        type:'POST',
+        data : {},
+        success: function (response){
+            let PLANESESTUDIO =JSON.parse(response);
+            console.log(PLANESESTUDIO);
+            let template="";
+            PLANESESTUDIO.forEach(plan => {
+              /*if(plan.maximo_materias!=""){
+                maximomaterias= plan.maximo_materias;
+              } else{
+                maximomaterias="Ilimitado";
+              }*/
+              let maximomaterias= plan.maximo_materias==null ? "ILIMITADO" : plan.maximo_materias;
+              template +=`
+              <tr class="text-center">
+                  <td data-label="Clave">${plan.id_plan}</td>
+                  <td data-label="Nombre">${plan.nombre_plan}</td>
+                  <td data-label="Duración">${plan.semestres}</td>
+                  <td data-label="Limite de Inscr. por periodo">${maximomaterias}</td>
+                  <td data-label="Creditos Oblig.">${plan.creditos_obligatorios}</td>
+                  <td data-label="Creditos Opt.">${plan.creditos_optativos}</td>
+                  <td data-label="Creditos Tot.">${plan.creditos_totales}</td>
+                  <td data-label="Acciones">
+                      <button type="button" title="Editar Plan" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#Edit_Plan_Modal"><i class='bx bxs-pencil'></i></button>
+                      <button type="button" title="Eliminar Plan" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal_baja_Plan"><i class='bx bx-trash'></i></button>
+                  </td>
+              </tr>
+              `;
+            });
+            $("#tbl-plan-estudios").html(template);
+            }
     });
 }
--- APP/view/plan_de_estudios.php
+++ APP/view/plan_de_estudios.php
@@ -4,14 +4,14 @@
 <?php $titulo="Planes de estudio | SIFESC";
 include_once "./includes/header.php";?>
 <body id="body">
-<?php include "./includes/sidebar2.php";?>    
+<?php include "./includes/sidebar2.php";?>
     <!--              AQUI VA EL CONTENIDO PARA AGREGAR PLANES DE ESTUIDIO               -->
     <div class="home_content2">
         <div class="texthome">
             <div class="mb-4">
                 <div class="grid">
                     <div class="">
-                        <a href="./index.php" title="Volver"> 
+                        <a href="./index.php" title="Volver">
                             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi    bi-arrow-left-circle" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                             </svg>
@@ -32,9 +32,9 @@ include_once "./includes/header.php";?>
                             </div>
                             <p id="ampm" class="ampm"></p>
                         </div>
-                        <!-- RELOJ FUNCIONAL Y BONITO ------------------------------------------------------------ -->      
+                        <!-- RELOJ FUNCIONAL Y BONITO ------------------------------------------------------------ -->
                     </div>
-                </div>    
+                </div>
                 <p class="text-center mb-3">Agrega los planes de estudio existentes.</p>
                 <div class="d-grid gap-2 col-3 mx-auto">
                     <div class="modal-content">
@@ -63,7 +63,7 @@ include_once "./includes/header.php";?>
 
                     </tr>
                 </thead>
-                <tbody>
+                <tbody id="tbl-plan-estudios">
                     <tr class="text-center">
                         <td data-label="Clave">0017</td>
                         <td data-label="Nombre">
@@ -75,7 +75,7 @@ include_once "./includes/header.php";?>
                         <td data-label="Creditos Tot.">431</td>
                         <td data-label="Acciones">
                             <button type="button" title="Editar Plan" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#Edit_Plan_Modal"><i class='bx bxs-pencil'></i></button>
-                            <button type="button" title="Eliminar Plan" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal_baja_Plan"><i class='bx bx-trash'></i></button> 
+                            <button type="button" title="Eliminar Plan" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal_baja_Plan"><i class='bx bx-trash'></i></button>
                         </td>
                     </tr>
                     <tr class="text-center">
@@ -102,7 +102,7 @@ include_once "./includes/header.php";?>
                         <td data-label="Creditos Tot.">431</td>
                         <td data-label="Acciones">
                             <button type="button" title="Editar Plan" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#Edit_Plan_Modal"><i class='bx bxs-pencil'></i></button>
-                            <button type="button" title="Eliminar Plan" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal_baja_Plan"><i class='bx bx-trash'></i></button> 
+                            <button type="button" title="Eliminar Plan" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modal_baja_Plan"><i class='bx bx-trash'></i></button>
                         </td>
                     </tr>
                 </tbody>
@@ -112,11 +112,12 @@ include_once "./includes/header.php";?>
             <div class="d-grid gap-2 col-3 mx-auto">
                 <p class="mt-3 mb-2"></p>
             </div>
-        </div> 
+        </div>
     </div>
 
     <!-- JavaScripts-->
     <?php include_once "./includes/js.php"; ?>
+    <script src="../service/lista-plandeestudios.js"></script>
     <!-- Modales -->
     <?php include "./modal/modal-agregar-plan-estu.php"; ?>
     <?php include "./modal/modal-edit-plan-estu.php"; ?>
