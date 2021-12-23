b6c28067e2554f5e257ea900f191ce2b020b42ed
NayelliSZ <91213591+NayelliSZ@users.noreply.github.com>
2021-12-22 23:46:49 -0600

    seleccion carrera

--- index.php
+++ index.php
@@ -105,7 +105,6 @@ if(isset($_SESSION['usuario']) && $_SESSION['sessionSuccess'])
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- PHP DE MODALES -->
     <?php include "./APP/view/modal/modal-valida-datos-recuperacion.php"; ?>
-    <?php include ".APP/view/modal/modal-aviso-olvide-contraseña.php"; ?>
     <script src="./APP/service/login-ajax.js"></script>
     <script src="./APP/service/recupera-contra.js"></script>
   </body>
