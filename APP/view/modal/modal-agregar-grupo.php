5dd9ef419acd8419aa5c26526a80d970f8b3ad21
EmmanuelGntleman <emmanuel_th_2698@hotmail.com>
2021-12-26 20:09:19 -0600

    Grupos

---
+++ APP/view/modal/modal-agregar-grupo.php
@@ -0,0 +1,30 @@
+<!-- Modal AGREGAR GRUPO -->
+
+          
+<div class="modal fade" id="agrega_grupo" tabindex="-1" aria-labelledby="agrega_grupo" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form id="frm_m_a_grupo">
+        <div class="modal-header">
+          <h5 class="modal-title" id="agrega_grupo_h5">Agregar grupo</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div>
+            <p>Escribe el grupo que deseas agregar</p>
+          </div>
+          <div class="mt-3 mb-2"></div>
+          <div class="form-group" id="actual-pass-group">
+            <div class="form-group">
+              <input type="text" placeholder="Grupo: " class="form-control" style="height: 60px" id="grupo_agrega" name="grupo_agrega">
+            </div>
+          </div>
+        </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-primary">Agregar</button>
+      </div>
+    </form>
+  </div>
+</div>
+</div>
