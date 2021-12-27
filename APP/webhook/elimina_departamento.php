fa5f0a11e593979d5f4d47b82c4cdde252e65592
Bauther <91104313+Bauther@users.noreply.github.com>
2021-12-26 20:51:58 -0600

    Aavance add, mod, elim
    Co-Authored-By: NayelliSZ <91213591+NayelliSZ@users.noreply.github.com>

--- APP/webhook/elimina_departamento.php
+++ APP/webhook/elimina_departamento.php
@@ -1,6 +1,6 @@
 <?php 
 include_once"../control/controlDepartamento.php";
-$id_departamento=$_POST['id_departamento'];
+$id_departamento=$_POST['idDepartamento'];
 $estatus=$_POST['estatus'];
 $result=deleteDepartamento($id_departamento,$estatus);
 echo $result;	
