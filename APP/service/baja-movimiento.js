5f3f3f40e3ade4ff071fb495a3c6c937ee2c3d66
NayelliSZ <91213591+NayelliSZ@users.noreply.github.com>
2021-12-30 14:17:45 -0600

    inscripcion
    restar 1 a inscritos cuando das de baja la materia

--- APP/service/baja-movimiento.js
+++ APP/service/baja-movimiento.js
@@ -1,30 +1,50 @@
 $(document).ready(function(){
     let id_inscripcion=0;
+    let inscritos=0;
+    let cupo=0;
     $("#frm_baja_asignatura_inscrip").on("submit", function(e){
         var formData = new FormData(document.getElementById("frm_baja_asignatura_inscrip"));
-    formData.append("idInscripcion",$("#idInscripcionAlumno").text());
-    formData.append("idAsignacion", $("#idAsignacion").val());
-    formData.append("estatus", "2");
-    id_inscripcion=$("#idInscripcionAlumno").text();
-    $.ajax({
-        url: "../webhook/modifica_movimiento.php",
-        type: 'POST',
-        data: formData,
-        dataType: "html",
-        cache: false,
-        contentType: false,
-        processData: false
-    })
+        formData.append("idInscripcion",$("#idInscripcionAlumno").text());
+        formData.append("idAsignacion", $("#idAsignacion").val());
+        formData.append("estatus", "2");
+        id_inscripcion=$("#idInscripcionAlumno").text();
+        $.ajax({
+            url: "../webhook/modifica_movimiento.php",
+            type: 'POST',
+            data: formData,
+            dataType: "html",
+            cache: false,
+            contentType: false,
+            processData: false
+        })
         .done(function(res){
-            listaMovimientos(id_inscripcion);
-        $("#frm_baja_asignatura_inscrip").trigger('reset');
-        $("#Modal_baja").modal('hide');
+            console.log(res);
+            if(res==1){
+                inscritos=parseInt($("#ins").text())-1;
+                console.log($("#ins").val());
+                var formData = new FormData(document.getElementById("frm_baja_asignatura_inscrip"));
+                formData.append("idAsignacion", $("#grupo").val());
+                formData.append("inscritos", inscritos);
+                $.ajax({
+                    url: "../webhook/modifica_inscritos_asignacion.php",
+                    type: 'POST',
+                    data: formData,
+                    dataType: "html",
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                })
+                .done(function(res){
+                    console.log(res);
+                    listaMovimientos(id_inscripcion);
+                    $("#frm_baja_asignatura_inscrip").trigger('reset');
+                    $("#Modal_baja").modal('hide');
+                });
+            }
         });
-        
-    e.preventDefault();
+        e.preventDefault();
+    });
 });
-});
-
 
 //esta es una funcion para eliminar el movimiento
 function bajaMovimiento(idAsignacion){
