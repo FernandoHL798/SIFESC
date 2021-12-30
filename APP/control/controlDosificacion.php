4e55d9f41d060f00e195945ad8d6f9301a975236
Fernando <lfernandohl901@gmail.com>
2021-12-29 21:23:00 -0600

    Avance dosificacion

--- APP/control/controlDosificacion.php
+++ APP/control/controlDosificacion.php
@@ -35,3 +35,29 @@ function deleteDosificacion($params){
     $DOSIFICACION->setIdPeriodoFk($params['id_periodo_fk']);
     return $DOSIFICACION->queryDeleteDosificacion();
 }
+
+function consultaDosificacionPeriodo($idUsuario,$idPeriodo){
+    include_once "../model/DOSIFICACION.php";
+    $DOSIFICACION = new DOSIFICACION();
+    //Verificamos que exista la dosificacion con el ultimo periodo
+    $obj_dosificacion= $DOSIFICACION->queryconsultaDosificacionPeriodo($idUsuario,$idPeriodo);
+    //Si existe, retornará ese mismo valor que esta en la DB
+    if(count($obj_dosificacion)>0){
+        return json_encode($obj_dosificacion);
+    } else{
+        //Sino, generaremos la dosificacion con los seteos para la dosificacion 
+        //Generar el algoritmo para la fecha y hora de inscripcion
+        $DOSIFICACION->setIdUsuarioAlumnoFk($idUsuario);
+        $DOSIFICACION->setIdPeriodoFk($idPeriodo);
+        $DOSIFICACION->setFechaInscripcion();
+        $DOSIFICACION->setHoraInscripcion();
+        //Agregamos la dosificacion
+        $result= $DOSIFICACION->queryInsertDosificacion();
+        //Si la agregacion fue correcta, retornamos la informacion
+        if($result){
+            $obj_dosificacion= $DOSIFICACION->queryconsultaDosificacionPeriodo($idUsuario,$idPeriodo);
+            return json_encode($obj_dosificacion);
+        }
+        
+    }
+}
---
+++ APP/service/control-dosificacion.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    getPeriodo();
+    verificaDosificacion();
+});
+
+function getPeriodo(){
+    $.ajax({
+        url: "../webhook/consultaPeriodo-dosificacion.php",
+        type: 'POST',
+        data : {   },
+        success: function (response) {
+            //Convertimos el string a JSON
+            let PERIODOS = JSON.parse(response); 
+            let template="";
+            console.log(PERIODOS);
+            $("#nombre_periodo").html(PERIODOS[0].periodo);
+            getDatosAlumno();
+            }
+        });   
+}
+
+function getDatosAlumno(){
+    $.ajax({
+        url: "../webhook/lista-alumno-dosificacion.php",
+        type: 'POST',
+        data : {   idUsuario: $("#idUsuario").val(),
+        			idPlan: $("#idPlan").val()     },
+        success: function (response) {
+            //Convertimos el string a JSON
+            console.log(response);
+            let ALUMNOS = JSON.parse(response); 
+            let template="";
+            console.log(ALUMNOS);
+            let alumno= ALUMNOS[0];
+            console.log(alumno.cuenta_alumno);
+            $("#tbl-noCuenta").html(alumno.cuenta_alumno);
+            $("#tbl-nombreAlumno").html(alumno.nombre_alumno+' '+alumno.primer_apellido+' '+ alumno.segundo_apellido);
+            let turno = alumno.turno>1 ? "Vespertino" : "Matutino";
+            $("#tbl-Turno").html(turno);
+            $("#tbl-Carrera").html(alumno.id_carrera+' - '+alumno.nombre_carrera);
+            }
+        });   
+}
